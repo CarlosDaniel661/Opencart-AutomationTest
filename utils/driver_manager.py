986fa52f0feb25c389fd14bcf385fe744563f919
CarlosDaniel661 <timonrock2015@gmail.com>
2025-03-21 00:41:39 -0300

    driver_manager chromedriver

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -4,21 +4,30 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import tempfile
 import platform
+import shutil
+import os
 
 def get_driver():
     options = Options()
-    # options.add_argument("--headless")  # Descomenta para ejecutar en modo headless
+    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--allow-insecure-localhost")
     options.add_argument("--remote-debugging-port=9222")
-
-    # Crear un directorio temporal único para los datos del usuario
     user_data_dir = tempfile.mkdtemp()
     options.add_argument(f"--user-data-dir={user_data_dir}")
 
+    cache_dir = os.path.expanduser("~/.wdm")
+    if os.path.exists(cache_dir):
+        shutil.rmtree(cache_dir)
+        print(f"Eliminada la caché de webdriver-manager en: {cache_dir}")
+
     # Usar webdriver-manager para instalar ChromeDriver automáticamente
     chromedriver_path = ChromeDriverManager().install()
+    print(f"ChromeDriver descargado en: {chromedriver_path}")  # Imprimir la ruta
+
+    os.chmod(chromedriver_path, stat.S_IRWXU)
+    print(f"Permisos de ejecución dados a: {chromedriver_path}")
 
     # Configurar el servicio de ChromeDriver
     service = Service(chromedriver_path)
